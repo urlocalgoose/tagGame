9d985a22cff6e2378a43cc0a7affbb020f652d40
urlocalgoose <urlocalgoose@gmail.com>
2021-03-13 18:03:09 -0500

    Basics

---
+++ Assets/Scipts/Player.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizantal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizantal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update() {
+        HandleMovement();
+    }
+}
